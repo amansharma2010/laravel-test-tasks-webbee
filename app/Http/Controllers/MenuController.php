28bb51bd7bc9f373fd912bc6b75334f84f15951f
Aman Sharma <aman.01rad@gmail.com>
2021-05-17 20:13:18 +0530

    Test Task 3

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -95,6 +95,8 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        $menuModel = new MenuItem();
+        $menuItems = $menuModel->getNestedMenuItems();
+        return response()->json($menuItems);
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
-
+    public function getNestedMenuItems($menuId = null) {
+        $menuItems = $this->where('parent_id', $menuId)->get()->toArray();
+        $tempArray = [];
+        foreach ($menuItems as $singleMenu) {
+            $singleMenu['children'] = $this->getNestedMenuItems($singleMenu['id']);
+            $tempArray[] = $singleMenu;
+        }
+        $menuItems = $tempArray;
+        
+        return $menuItems;
+    }
 }
